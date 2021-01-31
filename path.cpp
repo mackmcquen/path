6f4905ddb512a5445c6fabc650cce41d53523a5c
mackmcquen <mackmcquen@comcast.net>
2021-01-31 04:25:49 -0600

    First commit

---
+++ path.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main () {
+
+    int name;
+
+    std::cout << "Hello.  What is your name wanderer?\n";
+    std::cin >> name;
+    std::cout << name << "? Alright then.  Follow me.\n";
+}
